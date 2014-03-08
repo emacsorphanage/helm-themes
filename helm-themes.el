ba2b86113ed3cd354af88e6713a7ccd4473e5edf
Syohei YOSHIDA <syohex@gmail.com>
2014-03-08 09:32:46 +0900

    Bump up version and update Changes

--- helm-themes.el
+++ helm-themes.el
@@ -1,10 +1,10 @@
 ;;; helm-themes.el --- Color theme selection with helm interface -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2013 by Syohei YOSHIDA
+;; Copyright (C) 2014 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-themes
-;; Version: 0.02
+;; Version: 0.03
 ;; Package-Requires: ((helm "1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
